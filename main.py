9ee7d148f46001e51aba9cf6d702eb0a1af01a01
Siddharth Nahar <77606988+sidd68-hub@users.noreply.github.com>
2026-07-29 23:53:51 +0530

    Search Functionality Added

--- main.py
+++ main.py
@@ -26,7 +26,20 @@ def generate_passoword():
     pyperclip.copy(password)
 
 
-
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:                
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website,message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website} exists.")    
 
 
 
@@ -96,27 +109,25 @@ canvas.grid(row=0, column=1)
 #Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "angela@gmail.com")
-
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password",command=generate_passoword)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", command=generate_passoword)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
